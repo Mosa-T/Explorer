8f0e3e735fa965f7663dad4bb941e08ea61535a6
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2022-01-27 17:54:08 +0200

    Update FileExplore.java

--- FileExplore.java
+++ FileExplore.java
@@ -1,91 +1,58 @@
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.ChannelExec;
+import java.util.Vector;
+import com.jcraft.jsch.*;
 
 public class FileExplore {
 
-    //Look through the files/folders, if it's a file, print it, if it's a directory, print it and recurse through it
-    //And do the same for its files/folders.
-    static void explore(File[] list)
-    {
-        for (File i : list) {
-            if (i.isFile())
-                System.out.println("FILE:  - "+ i + "\\" + i.getName());
-
-            else if (i.isDirectory()) {
-                System.out.println("FOLDER:- "+ i );
-                explore(i.listFiles());
-            }
-        }
-    }
-    //Pick a directory to explore, if it exists and is a valid directory, we will explore it.
     public static void main(String[] args)  {
+
+        if(args.length != 5){
+            System.out.println("Incorrect argument input:- exiting");
+            System.exit(0);
+        }
 
         String username="pi";
         String password="123";
-        //String host="192.162.1.110"; //or 255
-        //String host="192.168.1.0";
         String host="192.168.1.110";
         int port=22;
-        String command="ls ";
+        String directory="/home/pi/";
         
         Session session = null;
-        ChannelExec channel = null;
-        System.out.println("TRYING");
+        ChannelSftp channel = null;
+        System.out.println("Agent:- Attempting to connect");
         try {
             session = new JSch().getSession(username, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
             session.connect();
-            
-            System.out.println("CONNECTED_1");
-            channel = (ChannelExec) session.openChannel("exec");
-            channel.setCommand(command);
-            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
-            channel.setOutputStream(responseStream);
+            System.out.println("Agent:- Connection to remote host <" + username +"> establish");
+
+            System.out.println("Agent:- Establishing SSH File Transfer Protocol channel");
+            channel = (ChannelSftp) session.openChannel("sftp");
             channel.connect();
-            System.out.println("CONNECTED_2");
-            while (channel.isConnected()) {
-                Thread.sleep(100);
+            System.out.println("Agent:- Established sftp channel");
+
+            channel.cd(directory);
+            Vector filelist = channel.ls(directory);
+            System.out.println("Agent:- File data transferred through channel");
+
+            for(int i=0; i<filelist.size();i++){
+                ChannelSftp.LsEntry entry = (ChannelSftp.LsEntry) filelist.get(i);
+                System.out.println(entry.getFilename());
+                //System.out.println(filelist.get(i).toString());
             }
-            System.out.println("Slept");
-            String responseString = new String(responseStream.toByteArray());
-            System.out.println(responseString);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         } catch (JSchException e) {
+            e.printStackTrace();
+        } catch (SftpException e) {
             e.printStackTrace();
         } finally {
             if (session != null) {
+                System.out.println("Agent:- closing session");
                 session.disconnect();
             }
             if (channel != null) {
+                System.out.println("Agent:- closing channel");
                 channel.disconnect();
             }
         }
-        
-        
-        
-        if(args.length != 2){
-            System.out.println("Incorrect argument input:- exiting");
-            System.exit(0);
-        }
-        File f_dir = new File(args[1]);
-        System.out.println("Remote hostname:- "+args[0]);
-        System.out.println("Directory request:- " + args[1]);
-        if (f_dir.exists() && f_dir.isDirectory()) {
-            File list[] = f_dir.listFiles();
-            explore(list);
-        } else{
-            System.out.println("Error in:- Directory request");
-        }
     }
 }
-
-
-
-
