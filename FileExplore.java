8b275123cfd276eaef5304031c90b9da73a465c5
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2022-01-27 12:58:57 +0200

    Update FileExplore.java

--- FileExplore.java
+++ FileExplore.java
@@ -1,5 +1,11 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import com.jcraft.jsch;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.ChannelExec;
+
 public class FileExplore {
 
     //Look through the files/folders, if it's a file, print it, if it's a directory, print it and recurse through it
@@ -17,8 +23,41 @@ public class FileExplore {
         }
     }
     //Pick a directory to explore, if it exists and is a valid directory, we will explore it.
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws JSchException {
+
+        String username; String password; String host; int port; String command;
+        Session session = null;
+        ChannelExec channel = null;
+
+        try {
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+
+            while (channel.isConnected()) {
+                Thread.sleep(100);
+            }
+
+            String responseString = new String(responseStream.toByteArray());
+            System.out.println(responseString);
+        } finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
+        
+        
+        
         if(args.length != 2){
             System.out.println("Incorrect argument input:- exiting");
             System.exit(0);
@@ -34,3 +73,4 @@ public class FileExplore {
         }
     }
 }
+
